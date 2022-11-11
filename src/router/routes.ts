0d17f4f6f00373f2b9f408a54600f22fd1021d99
Reisliebhaber <pmntt21@gmail.com>
2022-11-11 10:49:20 +0100

    #1:  add script to run quasar app and add new test route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,6 +6,12 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
+  {
+    path: '/test',
+    name:'test',
+    component: () => import('layouts/MainLayout.vue'),
+    //children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
